61ac6ee585e9a5fdf325b7acb51bf8bd04d835fa
Heinhtet2737 <149848424+Heinhtet2737@users.noreply.github.com>
2023-11-08 04:09:48 -0800

    Add files via upload

--- basket/storage.py
+++ basket/storage.py
@@ -7,186 +7,123 @@
 # LICENSE file in the root directory of this source tree.
 from __future__ import unicode_literals
 
-import abc
-import six
+import logging
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
-from shuup.core.models import Basket
-from shuup.core.utils.users import real_user_or_none
-from shuup.utils.importing import cached_load
+from shuup.core.basket.storage import BaseDatabaseBasketStorage, BasketStorage
+from shuup.front.models import StoredBasket
+from shuup.utils.analog import LogEntryKind
+
+LOGGER = logging.getLogger(__name__)
 
 
-class BasketCompatibilityError(Exception):
-    pass
-
-
-class BasketStorage(six.with_metaclass(abc.ABCMeta)):
-    def load(self, basket):
-        """
-        Load the given basket's data dictionary from the storage.
-
-        :type basket: shuup.core.basket.objects.BaseBasket
-        :rtype: dict
-        :raises:
-          `BasketCompatibilityError` if basket loaded from the storage
-          is not compatible with the requested basket.
-        """
-        stored_basket = self._load_stored_basket(basket)
-        if not stored_basket:
-            return {}
-        if stored_basket.shop_id != basket.shop.id:
-            msg = "Error! Cannot load basket of a different Shop (" "%s id=%r with Shop=%s, Dest. Basket Shop=%s)" % (
-                type(stored_basket).__name__,
-                stored_basket.id,
-                stored_basket.shop_id,
-                basket.shop.id,
+class DirectSessionBasketStorage(BasketStorage):
+    def __init__(self):
+        if settings.SESSION_SERIALIZER == "django.contrib.sessions.serializers.JSONSerializer":  # pragma: no cover
+            raise ImproperlyConfigured(
+                "Error! `DirectSessionBasketStorage` will not work with the JSONSerializer session serializer."
             )
-            raise BasketCompatibilityError(msg)
-        price_units_diff = _price_units_diff(stored_basket, basket.shop)
-        if price_units_diff:
-            msg = "Error! %s %r: Price unit mismatch with Shop (%s)" % (
-                type(stored_basket).__name__,
-                stored_basket.id,
-                price_units_diff,
-            )
-            raise BasketCompatibilityError(msg)
-        return stored_basket.data or {}
-
-    @abc.abstractmethod
-    def _load_stored_basket(self, basket):
-        """
-        Load stored basket for the given basket from the storage.
-
-        The returned object should have ``id``, ``shop_id``,
-        ``currency``, ``prices_include_tax`` and ``data`` attributes.
-
-        :type basket: shuup.core.basket.objects.BaseBasket
-        :return: Stored basket or None
-        """
-        pass
-
-    @abc.abstractmethod
-    def save(self, basket, data):  # pragma: no cover
-        """
-        Save the given data dictionary into the storage for the given basket.
-
-        :type basket: shuup.core.basket.objects.BaseBasket
-        :type data: dict
-        :rtype str:
-        :return: The unique identifier of the basket just created
-        """
-        pass
-
-    @abc.abstractmethod
-    def delete(self, basket):  # pragma: no cover
-        """
-        Delete the basket from storage.
-
-        :type basket: shuup.core.basket.objects.BaseBasket
-        """
-        pass
-
-    def finalize(self, basket):
-        """
-        Mark the basket as "finalized" (i.e. completed) in the storage.
-
-        The actual semantics of what finalization does are up to each backend.
-
-        :type basket: shuup.core.basket.objects.BaseBasket
-        """
-        self.delete(basket=basket)
-
-    def basket_exists(self, key, shop):
-        """
-        Check if basket exists in the storage.
-
-        For example this is used from API to check whether the basket
-        actually exists for certain shop when accessed with key.
-
-        :type key: str
-        :type shop: shuup.core.models.Shop
-        """
-        return False
-
-
-class BaseDatabaseBasketStorage(BasketStorage):
-    model = Basket
-
-    def get_basket_kwargs(self, basket):
-        return {}
 
     def save(self, basket, data):
-        """
-        :type basket: shuup.core.basket.objects.BaseBasket
-        """
-        stored_basket = self._load_stored_basket(basket)
-        stored_basket.data = data
-        stored_basket.taxless_total_price = basket.taxless_total_price_or_none
-        stored_basket.taxful_total_price = basket.taxful_total_price_or_none
-        stored_basket.product_count = basket.smart_product_count
-        stored_basket.customer = basket.customer or None
-        stored_basket.orderer = basket.orderer or None
-        stored_basket.creator = real_user_or_none(basket.creator)
-        if hasattr(self.model, "supplier") and hasattr(basket, "supplier"):
-            stored_basket.supplier = basket.supplier
-
-        stored_basket.class_spec = "%s.%s" % (basket.__class__.__module__, basket.__class__.__name__)
-
-        stored_basket.save()
-        stored_basket.products.set(set(basket.product_ids))
-        return stored_basket
-
-    def delete(self, basket):
-        stored_basket = self._load_stored_basket(basket)
-        if stored_basket and stored_basket.pk:
-            stored_basket.deleted = True
-            stored_basket.save()
-
-    def finalize(self, basket):
-        stored_basket = self._load_stored_basket(basket)
-        if stored_basket and stored_basket.pk:
-            stored_basket.deleted = True
-            stored_basket.finished = True
-            stored_basket.save()
+        stored_basket = DictStoredBasket.from_basket_and_data(basket, data)
+        basket.request.session[basket.basket_name] = stored_basket.as_dict()
 
     def _load_stored_basket(self, basket):
-        basket_get_kwargs = self.get_basket_kwargs(basket)
-        stored_basket = None
-        if basket_get_kwargs:
-            stored_basket = self.model.objects.filter(deleted=False, **basket_get_kwargs).first()
-        if not stored_basket:
-            stored_basket = self.model(
-                shop=basket.shop,
-                currency=basket.currency,
-                prices_include_tax=basket.prices_include_tax,
-            )
+        stored_basket_dict = basket.request.session.get(basket.basket_name)
+        if not stored_basket_dict:
+            return None
+        return DictStoredBasket.from_dict(stored_basket_dict)
+
+    def delete(self, basket):
+        basket.request.session.pop(basket.basket_name, None)
+
+
+class DictStoredBasket(object):
+    def __init__(self, id, shop_id, currency, prices_include_tax, data):
+        self.id = id
+        self.shop_id = shop_id
+        self.currency = currency
+        self.prices_include_tax = prices_include_tax
+        self.data = data or {}
+
+    @classmethod
+    def from_basket_and_data(cls, basket, data):
+        return cls(
+            id=(getattr(basket, "id", None) or basket.basket_name),
+            shop_id=basket.shop.id,
+            currency=basket.currency,
+            prices_include_tax=basket.prices_include_tax,
+            data=data,
+        )
+
+    @classmethod
+    def from_dict(cls, mapping):
+        return cls(**mapping)
+
+    def as_dict(self):
+        return {
+            "id": self.id,
+            "shop_id": self.shop_id,
+            "currency": self.currency,
+            "prices_include_tax": self.prices_include_tax,
+            "data": self.data,
+        }
+
+
+class DatabaseBasketStorage(BaseDatabaseBasketStorage):
+    model = StoredBasket
+
+    def _get_session_key(self, basket):
+        return "basket_%s_key" % basket.basket_name
+
+    def get_basket_kwargs(self, basket):
+        # Lets first try to get basket kwargs from basket request session
+        basket_kwargs = basket.request.session.get(self._get_session_key(basket))
+        if not basket_kwargs:
+            # Fallback to basket customer and key combination
+            basket_kwargs = {"key": basket.key}
+        return basket_kwargs
+
+    def save(self, basket, data):
+        stored_basket = super(DatabaseBasketStorage, self).save(basket, data)
+        basket_kwargs = {"pk": stored_basket.pk, "key": stored_basket.key}
+        basket.request.session[self._get_session_key(basket)] = basket_kwargs
+
+    def delete(self, basket):
+        super(DatabaseBasketStorage, self).delete(basket)
+        basket.request.session.pop(self._get_session_key(basket), None)
+
+    def finalize(self, basket):
+        super(DatabaseBasketStorage, self).finalize(basket)
+        basket.request.session.pop(self._get_session_key(basket), None)
+
+    def _load_stored_basket(self, basket):
+        stored_basket = super(DatabaseBasketStorage, self)._load_stored_basket(basket)
+        if not stored_basket.pk and self.get_basket_kwargs(basket):
+            basket.request.session.pop(self._get_session_key(basket), None)
         return stored_basket
 
     def basket_exists(self, key, shop):
         return self.model.objects.filter(key=key, shop=shop).exists()
 
+    def _get_key_for_logs(self, basket):
+        basket_kwargs = self.get_basket_kwargs(basket)
+        return "%s%s" % ("stored_basket_key:", basket_kwargs["key"])
 
-class DatabaseBasketStorage(BaseDatabaseBasketStorage):
-    def get_basket_kwargs(self, basket):
-        if basket.key:
-            return {"key": basket.key}
-        return {}
+    def add_log_entry(self, basket, message, extra={}, kind=LogEntryKind.NOTE):
+        try:
+            if getattr(basket, "shop", None):
+                identifier = self._get_key_for_logs(basket)
+                basket.shop.add_log_entry(kind=kind, identifier=identifier, message=message, extra=extra)
+        except Exception:
+            # This might get called on important checkout related flows it is not
+            # good idea to interrupt the business if for some reason this logging
+            # fails.
+            LOGGER.error("Adding log entry to stored basket failed.")
 
-
-def _price_units_diff(x, y):
-    diff = []
-    if x.currency != y.currency:
-        diff.append("currency: %r vs %r" % (x.currency, y.currency))
-    if x.prices_include_tax != y.prices_include_tax:
-        diff.append("includes_tax: %r vs %r" % (x.prices_include_tax, y.prices_include_tax))
-    return ", ".join(diff)
-
-
-def get_storage():
-    """
-    Retrieve a basket storage object.
-
-    :return: A basket storage object.
-    :rtype: BasketStorage
-    """
-    storage_class = cached_load("SHUUP_BASKET_STORAGE_CLASS_SPEC")
-    return storage_class()
+    def get_log_entries(self, basket):
+        identifier = self._get_key_for_logs(basket)
+        if getattr(basket, "shop", None):
+            return basket.shop.log_entries.filter(identifier=identifier)
+        return []
