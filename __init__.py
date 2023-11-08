5d7de94d0b7c9f17538ab8d8004227122d7a943c
Heinhtet2737 <149848424+Heinhtet2737@users.noreply.github.com>
2023-11-08 03:50:28 -0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -5,53 +5,28 @@
 #
 # This source code is licensed under the OSL-3.0 license found in the
 # LICENSE file in the root directory of this source tree.
-import django.conf
+from django.utils.translation import ugettext_lazy as _
 
-from shuup.apps import AppConfig
-from shuup.core.excs import MissingSettingException
-from shuup.utils import money
+import shuup.apps
 
 
-class ShuupCoreAppConfig(AppConfig):
-    name = "shuup.core"
-    verbose_name = "Shuup Core"
-    label = "shuup"  # Use "shuup" as app_label instead of "core"
-    required_installed_apps = (
-        "django.contrib.auth",
-        "django.contrib.contenttypes",
-        "easy_thumbnails",
-        "filer",
-    )
+# TODO: Document how to create custom pricing modules (Refs SHUUP-514)
+class CustomerGroupPricingAppConfig(shuup.apps.AppConfig):
+    name = __name__
+    verbose_name = _("Shuup Customer Group Pricing")
+    label = "shuup_customer_group_pricing"
     provides = {
-        "pricing_module": ["shuup.core.pricing.default_pricing:DefaultPricingModule"],
-        "order_source_validator": [
-            "shuup.core.order_creator:OrderSourceMinTotalValidator",
-            "shuup.core.order_creator:OrderSourceMethodsUnavailabilityReasonsValidator",
-            "shuup.core.order_creator:OrderSourceSupplierValidator",
+        "pricing_module": [__name__ + ".module:CustomerGroupPricingModule"],
+        "discount_module": [__name__ + ".module:CustomerGroupDiscountModule"],
+        "admin_product_form_part": [
+            __name__ + ".admin_form_part:CustomerGroupPricingFormPart",
+            __name__ + ".admin_form_part:CustomerGroupPricingDiscountFormPart",
         ],
-        "product_kind_specs": ["shuup.core.specs.product_kind:DefaultProductKindSpec"],
     }
 
     def ready(self):
-        from django.conf import settings
-
-        if not getattr(settings, "PARLER_DEFAULT_LANGUAGE_CODE", None):
-            raise MissingSettingException("PARLER_DEFAULT_LANGUAGE_CODE must be set.")
-        if not getattr(settings, "PARLER_LANGUAGES", None):
-            raise MissingSettingException("PARLER_LANGUAGES must be set.")
-
-        # set money precision provider function
-        from .models import get_currency_precision
-
-        money.set_precision_provider(get_currency_precision)
-
-        if django.conf.settings.SHUUP_ERROR_PAGE_HANDLERS_SPEC:
-            from .error_handling import install_error_handlers
-
-            install_error_handlers()
-
         # connect signals
-        import shuup.core.signal_handlers  # noqa: F401
+        import shuup.customer_group_pricing.signal_handers  # noqa F401
 
 
-default_app_config = "shuup.core.ShuupCoreAppConfig"
+default_app_config = __name__ + ".CustomerGroupPricingAppConfig"
