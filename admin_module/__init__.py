2b051a11d4583a215a420b8e1d07ce015f9260b5
Heinhtet2737 <149848424+Heinhtet2737@users.noreply.github.com>
2023-11-09 01:38:17 -0800

    Add files via upload

--- admin_module/__init__.py
+++ admin_module/__init__.py
@@ -7,77 +7,171 @@
 # LICENSE file in the root directory of this source tree.
 from __future__ import unicode_literals
 
-import datetime
-from django.db.models import Count, Sum
-from django.utils.timezone import now
+from django.template import engines
 from django.utils.translation import ugettext_lazy as _
+from django_jinja.backend import Jinja2
+from typing import Iterable
 
-from shuup.admin.base import AdminModule, MenuEntry
-from shuup.admin.dashboard import DashboardMoneyBlock
-from shuup.admin.menu import ORDERS_MENU_CATEGORY
-from shuup.admin.utils.urls import admin_url, derive_model_url
-from shuup.front.models import StoredBasket
-
-
-def get_unfinalized_cart_block(request, days=14):
-    days = int(days)
-
-    early_cutoff = now() - datetime.timedelta(days=days)
-    # The `hours` value for `late_cutoff` should maybe be tunable somehow.
-    # Either way, we're currently considering baskets abandoned if they've been
-    # unupdated for two hours.
-    late_cutoff = now() - datetime.timedelta(hours=2)
-
-    shop = request.shop
-    data = (
-        StoredBasket.objects.filter(shop=shop, currency=shop.currency)
-        .filter(updated_on__range=(early_cutoff, late_cutoff), product_count__gte=0)
-        .exclude(deleted=True, finished=True, persistent=True)
-        .aggregate(count=Count("id"), sum=Sum("taxful_total_price_value"))
-    )
-    if not data["count"]:
-        return
-
-    return DashboardMoneyBlock(
-        id="abandoned_carts_%d" % days,
-        color="red",
-        title=_("Abandoned Cart Value"),
-        value=(data.get("sum") or 0),
-        currency=shop.currency,
-        icon="fa fa-calculator",
-        subtitle=_("Based on {b} carts over the last {d} days").format(b=data.get("count"), d=days),
-    )
+from shuup.admin.base import AdminModule, MenuEntry, Notification
+from shuup.admin.menu import CONTENT_MENU_CATEGORY
+from shuup.admin.utils.object_selector import get_object_selector_permission_name
+from shuup.admin.utils.urls import admin_url, derive_model_url, get_edit_and_list_urls
+from shuup.admin.views.home import HelpBlockCategory, SimpleHelpBlock
+from shuup.utils.django_compat import reverse
+from shuup.xtheme._theme import get_current_theme
+from shuup.xtheme.engine import XthemeEnvironment
+from shuup.xtheme.models import Font, Snippet
 
 
-class CartAdminModule(AdminModule):
-    name = "Cart"
+class XthemeAdminModule(AdminModule):
+    """
+    Admin module for Xtheme.
 
-    def get_dashboard_blocks(self, request):
-        unfinalized_block = get_unfinalized_cart_block(request, days=14)
-        if unfinalized_block:
-            yield unfinalized_block
+    Allows theme activation/deactivation and further configuration.
+    """
 
-    def get_urls(self):
+    name = _("Shuup Extensible Theme Engine")
+    breadcrumbs_menu_entry = MenuEntry(_("Themes"), "shuup_admin:xtheme.config", category=CONTENT_MENU_CATEGORY)
+
+    def get_urls(self):  # doccov: ignore
         return [
-            admin_url(r"^carts/$", "shuup.front.admin_module.carts.views.CartListView", name="cart.list"),
             admin_url(
-                r"^carts/(?P<pk>\d+)/$", "shuup.front.admin_module.carts.views.CartDetailView", name="cart.detail"
+                r"^xtheme/guide/(?P<theme_identifier>.+?)/",
+                "shuup.xtheme.admin_module.views.ThemeGuideTemplateView",
+                name="xtheme.guide",
+            ),
+            admin_url(
+                r"^xtheme/configure/(?P<theme_identifier>.+?)/",
+                "shuup.xtheme.admin_module.views.ThemeConfigDetailView",
+                name="xtheme.config_detail",
+            ),
+            admin_url(
+                r"^xtheme/admin-configure/",
+                "shuup.xtheme.admin_module.views.AdminThemeConfigDetailView",
+                name="xtheme.admin_config_detail",
+            ),
+            admin_url(r"^xtheme/theme", "shuup.xtheme.admin_module.views.ThemeConfigView", name="xtheme.config"),
+        ]
+
+    def get_menu_entries(self, request):  # doccov: ignore
+        return [
+            MenuEntry(
+                text=_("Themes"),
+                icon="fa fa-paint-brush",
+                url="shuup_admin:xtheme.config",
+                category=CONTENT_MENU_CATEGORY,
+                ordering=1,
             ),
         ]
 
-    def get_menu_category_icons(self):
-        return {self.name: "fa fa-cart"}
+    def get_help_blocks(self, request, kind):
+        theme = getattr(request, "theme", None) or get_current_theme(request.shop)
+        if kind == "quicklink" and theme:
+            yield SimpleHelpBlock(
+                text=_("Customize the look and feel of your shop"),
+                actions=[
+                    {
+                        "text": _("Customize theme"),
+                        "url": reverse(
+                            "shuup_admin:xtheme.config_detail", kwargs={"theme_identifier": theme.identifier}
+                        ),
+                    }
+                ],
+                priority=200,
+                category=HelpBlockCategory.STOREFRONT,
+                icon_url="xtheme/theme.png",
+            )
 
-    def get_menu_entries(self, request):
+    def get_notifications(self, request):
+        try:
+            engine = engines["jinja2"]
+        except KeyError:
+            engine = None
+
+        if engine and isinstance(engine, Jinja2):  # The engine is what we expect...
+            if isinstance(engine.env, XthemeEnvironment):  # ... and it's capable of loading themes...
+                if not (getattr(request, "theme", None) or get_current_theme(request.shop)):
+                    # ... but there's no theme active?!
+                    # Panic!
+                    yield Notification(
+                        text=_("No theme is active. Click here to activate one."),
+                        title=_("Theming"),
+                        url="shuup_admin:xtheme.config",
+                    )
+
+
+class XthemeFontsAdminModule(AdminModule):
+    name = _("Shuup Extensible Theme Engine Fonts")
+    breadcrumbs_menu_entry = MenuEntry(_("Fonts"), "shuup_admin:xtheme.font.list", category=CONTENT_MENU_CATEGORY)
+
+    def get_urls(self):  # doccov: ignore
+        return [
+            admin_url(
+                r"^xtheme/font/$",
+                "shuup.xtheme.admin_module.views.FontListView",
+                name="xtheme.font.list",
+            ),
+            admin_url(
+                r"^xtheme/font/new/$",
+                "shuup.xtheme.admin_module.views.FontEditView",
+                name="xtheme.font.new",
+                kwargs={"pk": None},
+            ),
+            admin_url(
+                r"^xtheme/font/(?P<pk>\d+)/$",
+                "shuup.xtheme.admin_module.views.FontEditView",
+                name="xtheme.font.edit",
+            ),
+        ]
+
+    def get_menu_entries(self, request):  # doccov: ignore
         return [
             MenuEntry(
-                text=_("Carts"),
-                icon="fa fa-shopping-cart",
-                url="shuup_admin:cart.list",
-                category=ORDERS_MENU_CATEGORY,
-                aliases=[_("Show carts")],
+                text=_("Fonts"),
+                icon="fa fa-font",
+                url="shuup_admin:xtheme.font.list",
+                category=CONTENT_MENU_CATEGORY,
+                ordering=10,
             ),
         ]
 
     def get_model_url(self, object, kind, shop=None):
-        return derive_model_url(StoredBasket, "shuup_admin:cart", object, kind)
+        return derive_model_url(Font, "shuup_admin:xtheme.font", object, kind)
+
+    def get_extra_permissions(self) -> Iterable[str]:
+        return [get_object_selector_permission_name(Font)]
+
+    def get_permissions_help_texts(self) -> Iterable[str]:
+        return {get_object_selector_permission_name(Font): _("Allow the user to select fonts in admin.")}
+
+
+class XthemeSnippetsAdminModule(AdminModule):
+    name = _("Shuup Extensible Theme Engine Snippets")
+    breadcrumbs_menu_entry = MenuEntry(_("Snippets"), "shuup_admin:xtheme_snippet.list", category=CONTENT_MENU_CATEGORY)
+
+    def get_urls(self):
+        return get_edit_and_list_urls(
+            url_prefix=r"^xtheme/snippet",
+            view_template="shuup.xtheme.admin_module.views.Snippet%sView",
+            name_template="xtheme_snippet.%s",
+        ) + [
+            admin_url(
+                r"^xtheme/snippet/(?P<pk>\d+)/delete/$",
+                "shuup.xtheme.admin_module.views.SnippetDeleteView",
+                name="xtheme_snippet.delete",
+            )
+        ]
+
+    def get_menu_entries(self, request):
+        return [
+            MenuEntry(
+                text=_("Theme Custom CSS/JS"),
+                icon="fa fa-magic",
+                url="shuup_admin:xtheme_snippet.list",
+                category=CONTENT_MENU_CATEGORY,
+                ordering=2,
+            )
+        ]
+
+    def get_model_url(self, object, kind, shop=None):
+        return derive_model_url(Snippet, "shuup_admin:xtheme_snippet", object, kind)
--- migrations/0001_initial.py
+++ migrations/0001_initial.py
@@ -1,45 +1,37 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.conf import settings
+import enumfields.fields
 from django.db import migrations, models
 
 import shuup.core.fields
-import shuup.front.models.stored_basket
-import shuup.utils.properties
+import shuup.xtheme.models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('shuup', '0001_initial'),
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='StoredBasket',
+            name='SavedViewConfig',
             fields=[
                 ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
-                ('key', models.CharField(verbose_name='key', max_length=32, default=shuup.front.models.stored_basket.generate_key)),
-                ('created_on', models.DateTimeField(auto_now_add=True, verbose_name='created on', db_index=True)),
-                ('updated_on', models.DateTimeField(verbose_name='updated on', auto_now=True, db_index=True)),
-                ('persistent', models.BooleanField(verbose_name='persistent', db_index=True, default=False)),
-                ('deleted', models.BooleanField(verbose_name='deleted', db_index=True, default=False)),
-                ('finished', models.BooleanField(verbose_name='finished', db_index=True, default=False)),
-                ('title', models.CharField(verbose_name='title', max_length=64, blank=True)),
-                ('data', shuup.core.fields.TaggedJSONField(verbose_name='data')),
-                ('taxless_total_price_value', shuup.core.fields.MoneyValueField(blank=True, null=True, verbose_name='taxless total price', decimal_places=9, default=0, max_digits=36)),
-                ('taxful_total_price_value', shuup.core.fields.MoneyValueField(blank=True, null=True, verbose_name='taxful total price', decimal_places=9, default=0, max_digits=36)),
-                ('currency', shuup.core.fields.CurrencyField(max_length=4, verbose_name='currency')),
-                ('prices_include_tax', models.BooleanField(verbose_name='prices include tax')),
-                ('product_count', models.IntegerField(default=0, verbose_name='product_count')),
-                ('creator', models.ForeignKey(blank=True, related_name='baskets_created', to=settings.AUTH_USER_MODEL, null=True, verbose_name='creator', on_delete=models.CASCADE)),
-                ('customer', models.ForeignKey(blank=True, related_name='customer_baskets', to='shuup.Contact', null=True, verbose_name='customer', on_delete=models.CASCADE)),
-                ('orderer', models.ForeignKey(blank=True, related_name='orderer_baskets', to='shuup.PersonContact', null=True, verbose_name='orderer', on_delete=models.CASCADE)),
-                ('products', models.ManyToManyField(blank=True, verbose_name='products', to='shuup.Product')),
-                ('shop', models.ForeignKey(verbose_name='shop', to='shuup.Shop', on_delete=models.CASCADE)),
+                ('theme_identifier', models.CharField(db_index=True, max_length=64, verbose_name='theme identifier')),
+                ('view_name', models.CharField(db_index=True, max_length=64, verbose_name='view name')),
+                ('created_on', models.DateTimeField(verbose_name='created on', auto_now_add=True)),
+                ('status', enumfields.fields.EnumIntegerField(db_index=True, verbose_name='status', enum=shuup.xtheme.models.SavedViewConfigStatus)),
+                ('_data', shuup.core.fields.TaggedJSONField(db_column='data', default=dict, verbose_name='internal data')),
             ],
-            bases=(shuup.utils.properties.MoneyPropped, models.Model),
+        ),
+        migrations.CreateModel(
+            name='ThemeSettings',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('theme_identifier', models.CharField(db_index=True, max_length=64, verbose_name='theme identifier', unique=True)),
+                ('active', models.BooleanField(verbose_name='active', db_index=True, default=False)),
+                ('data', shuup.core.fields.TaggedJSONField(db_column='data', default=dict, verbose_name='data')),
+            ],
         ),
     ]
